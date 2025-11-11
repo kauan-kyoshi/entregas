088b8f91c078ab7e4897e036688e3f3244ee1ae6
Kauan Kyoshi Kubo Lima <kauankyoshik@gmail.com>
2025-11-11 17:16:35 -0300

    done

--- 02-push_swap-1/src/pull_b.c
+++ 02-push_swap-1/src/pull_b.c
@@ -1,37 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pull_b.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kakubo-l <kakubo-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 17:03:51 by kakubo-l          #+#    #+#             */
+/*   Updated: 2025/11/11 17:03:52 by kakubo-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/push_swap.h"
 
 void	move_to_top_b(t_stack *stack, int position)
 {
-    int		dist_top;
-    int		size;
+	int		dist_top;
+	int		size;
 
-    size = (int)stack->b->size;
-    if (position <= size / 2)
-    {
-        dist_top = position;
-        while (dist_top > 0)
-        {
-            rb(stack, 1);
-            dist_top--;
-        }
-    }
-    else
-    {
-        dist_top = size - position;
-        while (dist_top > 0)
-        {
-            rrb(stack, 1);
-            dist_top--;
-        }
-    }
+	size = (int)stack->b->size;
+	if (position <= size / 2)
+	{
+		dist_top = position;
+		while (dist_top > 0)
+		{
+			rb(stack, 1);
+			dist_top--;
+		}
+	}
+	else
+	{
+		dist_top = size - position;
+		while (dist_top > 0)
+		{
+			rrb(stack, 1);
+			dist_top--;
+		}
+	}
 }
 
 void	phase_pull_b_to_a(t_stack *stack, int size)
 {
-    while (size > 0)
-    {
-        move_to_top_b(stack, find_position(stack->b, size - 1));
-        pa(stack, 1);
-        size--;
-    }
+	while (size > 0)
+	{
+		move_to_top_b(stack, find_position(stack->b, size - 1));
+		pa(stack, 1);
+		size--;
+	}
 }
--- 02-push_swap-1/src/sort_many.c
+++ 02-push_swap-1/src/sort_many.c
@@ -14,41 +14,41 @@
 
 static void	phase_push_a_to_b(t_stack *stack, int chunk_size)
 {
-    int	pushed_count;
+	int	pushed_count;
 
-    pushed_count = 0;
-    while (stack->a->size > 0)
-    {
-        if (get_value(stack->a->head) <= pushed_count)
-        {
-            pb(stack, 1);
-            rb(stack, 1);
-            pushed_count++;
-        }
-        else if (get_value(stack->a->head) <= pushed_count + chunk_size)
-        {
-            pb(stack, 1);
-            pushed_count++;
-        }
-        else
-            ra(stack, 1);
-    }
+	pushed_count = 0;
+	while (stack->a->size > 0)
+	{
+		if (get_value(stack->a->head) <= pushed_count)
+		{
+			pb(stack, 1);
+			rb(stack, 1);
+			pushed_count++;
+		}
+		else if (get_value(stack->a->head) <= pushed_count + chunk_size)
+		{
+			pb(stack, 1);
+			pushed_count++;
+		}
+		else
+			ra(stack, 1);
+	}
 }
 
 void	sort_many(t_stack *stack)
 {
-    int		size;
-    int		chunks;
-    int		chunk_size;
+	int		size;
+	int		chunks;
+	int		chunk_size;
 
-    size = stack->a->size;
-    if (handle_small_cases(stack, size))
-        return ;
-    if (size <= 100)
-        chunks = 5;
-    else
-        chunks = 11;
-    chunk_size = size / chunks;
-    phase_push_a_to_b(stack, chunk_size);
-    phase_pull_b_to_a(stack, size);
+	size = stack->a->size;
+	if (handle_small_cases(stack, size))
+		return ;
+	if (size <= 100)
+		chunks = 5;
+	else
+		chunks = 11;
+	chunk_size = size / chunks;
+	phase_push_a_to_b(stack, chunk_size);
+	phase_pull_b_to_a(stack, size);
 }
